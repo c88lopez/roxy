7b4e23e0667cd0a06ddf8b4c710e744d496c59d2
Cristian Lopez <c88lopez@gmail.com>
2014-07-15 00:01:18 -0300

    Links y Datos
    - Se cambian los links por los dados en el email.
    - Se pulen algunas cosas del footer.

--- footer.php
+++ footer.php
@@ -24,13 +24,13 @@
                 <div class="widget">
                     <h5 class="widgetheading">Get in touch with us</h5>
                     <address>
-                        <strong>Moderna company Inc</strong><br>
-                        Modernbuilding suite V124, AB 01<br>
-                        Someplace 16425 Earth
+                        <strong>Roxana Bavaro</strong><br>
+                        <!--Modernbuilding suite V124, AB 01<br>
+                        Someplace 16425 Earth-->
                     </address>
                     <p>
-                        <i class="icon-phone"></i> (123) 456-7890 - (123) 555-7891 <br>
-                        <i class="icon-envelope-alt"></i> email@domainname.com
+                        <!--<i class="icon-phone"></i> (123) 456-7890 - (123) 555-7891 <br>-->
+                        <i class="icon-envelope-alt"></i> roxybavaro@gmail.com 
                     </p>
                 </div>
             </div>
@@ -38,11 +38,10 @@
                 <div class="widget">
                     <h5 class="widgetheading">Pages</h5>
                     <ul class="link-list">
-                        <li><a href="#">Press release</a></li>
-                        <li><a href="#">Terms and conditions</a></li>
-                        <li><a href="#">Privacy policy</a></li>
-                        <li><a href="#">Career center</a></li>
-                        <li><a href="#">Contact us</a></li>
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="portfolio.php">Portfolio</a></li>
+                        <li><a href="blog.php">Blog</a></li>
+                        <li><a href="contacto.php">Contacto</a></li>
                     </ul>
                 </div>
             </div>
@@ -74,17 +73,37 @@
                 <div class="col-lg-6">
                     <div class="copyright">
                         <p>
-                            <span>&copy; Moderna 2014 All right reserved. By </span><a href="http://bootstraptaste.com" target="_blank">Bootstraptaste</a>
+                            <!--<span>&copy; Moderna 2014 All right reserved. By </span><a href="http://bootstraptaste.com" target="_blank">Bootstraptaste</a>-->
                         </p>
                     </div>
                 </div>
                 <div class="col-lg-6">
                     <ul class="social-network">
-                        <li><a href="#" data-placement="top" title="Facebook"><i class="fa fa-facebook"></i></a></li>
-                        <li><a href="#" data-placement="top" title="Twitter"><i class="fa fa-twitter"></i></a></li>
-                        <li><a href="#" data-placement="top" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
-                        <li><a href="#" data-placement="top" title="Pinterest"><i class="fa fa-pinterest"></i></a></li>
-                        <li><a href="#" data-placement="top" title="Google plus"><i class="fa fa-google-plus"></i></a></li>
+                        <li>
+                            <a href="https://www.facebook.com/roxana.bavaro" data-placement="top" title="Facebook">
+                                <i class="fa fa-facebook"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://twitter.com/roxybavaro" data-placement="top" title="Twitter">
+                                <i class="fa fa-twitter"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="http://www.linkedin.com/profile/view?id=33355404&trk=nav_responsive_tab_profile" data-placement="top" title="Linkedin">
+                                <i class="fa fa-linkedin"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="http://es.pinterest.com/roxybavaro" data-placement="top" title="Pinterest">
+                                <i class="fa fa-pinterest"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="http://instagram.com/roxybavaro" data-placement="top" title="Instagram">
+                                <i class="fa fa-instagram"></i>
+                            </a>
+                        </li>
                     </ul>
                 </div>
             </div>
