a263567f0e1493e3d1302e1ff54040df9b56eea0
Cristian Lopez <c88lopez@gmail.com>
2014-09-14 18:53:55 -0300

    Se agregan iconos sociales abajo del titulo principal del site.

--- header.php
+++ header.php
@@ -27,7 +27,36 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="index.php"><span>Roxy</span>Bavaro</a>
+                <a class="navbar-brand" href="index.php"><span>Roxy</span> Bavaro</a>
+                <div class="col-lg-6" style="font-size: 16px;">
+                    <ul class="social-network">
+                        <li>
+                            <a href="https://www.facebook.com/roxana.bavaro" data-placement="top" title="Facebook">
+                                <i class="fa fa-facebook"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://twitter.com/roxybavaro" data-placement="top" title="Twitter">
+                                <i class="fa fa-twitter"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="http://www.linkedin.com/profile/view?id=33355404&trk=nav_responsive_tab_profile" data-placement="top" title="Linkedin">
+                                <i class="fa fa-linkedin"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="http://es.pinterest.com/roxybavaro" data-placement="top" title="Pinterest">
+                                <i class="fa fa-pinterest"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="http://instagram.com/roxybavaro" data-placement="top" title="Instagram">
+                                <i class="fa fa-instagram"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
             </div>
             <div class="navbar-collapse collapse ">
                 <ul class="nav navbar-nav">
